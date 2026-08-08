8751ee576d7994e0bb24e7ab84a4edf4f0d68293
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-08-08 17:14:39 +0530

    Implement lifespan management for FastAPI application with database table creation

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+from contextlib import asynccontextmanager  # noqa: I001
 from fastapi import FastAPI
 
-app = FastAPI()
+from database import create_tables
+
+@asynccontextmanager # with this we can control the lifespan of fastapi server like what task need to perform before server start or shutdown.
+async def lifespan(app: FastAPI):
+    create_tables()
+    print("Database tables created")
+    yield
+    # Shutdown
+    print("Shutting down the app")
+
+
+app = FastAPI(
+    title="Rangmanch Reviews API",
+    description="Theater reviews API for Rangmanch platform",
+    lifespan=lifespan
+)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Rangmanch Reviews API!"}
